bb332546175e44b457ecc3f7fbaf8e7ac6b3e8cb
alan <alanasto10@gmail.com>
2020-02-01 14:22:17 -0600

    se agrego la estructura general y el cuerpo de cada pagina

---
+++ Pagina/principal.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template ## permite renderisar templates
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('principal.html') # se agrega rende_template para usar el template
+
+@app.route('/busqueda')
+def busqueda():
+    return render_template('busqueda.html') # se agrega rende_template para usar el template
+
+@app.route('/resultadobusqueda')
+def resultado_busqueda():
+    return render_template('resultadobusqueda.html') # se agrega rende_template para usar el template
+
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html') # se agrega rende_template para usar el template
+
+@app.route('/resultadoconsulta')
+def resultado_consulta():
+    return render_template('resultadoconsulta.html') # se agrega rende_template para usar el template
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 8000)
+
