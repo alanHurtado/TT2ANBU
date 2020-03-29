f8bf1bd065fb40f9fa4a16aed9c63511ec9ebe2b
Daniel Lopez <isc.lopez95@gmail.com>
2020-03-28 18:56:13 -0600

    Modificación /recursos/DataBaseANBU.sql

--- Pagina/model/Bd_conect.py
+++ Pagina/model/Bd_conect.py
@@ -2,33 +2,30 @@ from flaskext.mysql import MySQL  ## permite el uso de MySQL
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey" 
 
-### Coneccion a la base de datos ####
+### Conexión a la base de datos ####
 app.config['MYSQL_DATABASE_USER'] = 'ANBU'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'ANBUTT2'
 app.config['MYSQL_DATABASE_DB'] = 'ANBU_DB'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL(app)
 
-app.secret_key = "mysecretkey" 
+conn = mysql.connect()  #conectamos a la BD
+cursor = conn.cursor()  #creamos cursos para manejar la BD
 
-def insertar_busqueda(in_name):
-    conn = mysql.connect()  # conectamos a la BD
-    cursor = conn.cursor()     # creamos cursos para manejar la BD
+def insertar_busqueda(in_name):    
     cursor.execute("INSERT INTO Busqueda (nombre_buscado) VALUES (%s)", (in_name))
+    cursor.lastrowid    
     print(conn.commit())
 
-def res_bus():
-    conn = mysql.connect()  # conectamos a la BD
-    cursor = conn.cursor()  
+def res_bus():    
     cursor.execute('SELECT * FROM Busqueda')
     data = cursor.fetchall()
     cursor.close()
     return data
 
-def res_per():
-    conn = mysql.connect()  # conectamos a la BD
-    cursor = conn.cursor()  
+def res_per(): 
     cursor.execute('SELECT * FROM Perfil')
     data = cursor.fetchall()
     cursor.close()
