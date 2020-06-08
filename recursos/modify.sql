be7a26ce43109041c898c088047f8b799cf8d738
Daniel Lopez <isc.lopez95@gmail.com>
2020-06-08 12:53:40 -0500

    Act DATABASE

--- recursos/modify.sql
+++ recursos/modify.sql
@@ -1,5 +1,16 @@
 USE anbuDB;
+-------------08/JUNIO/2020
+ALTER TABLE Arma
+MODIFY COLUMN porcentaje FLOAT;
 
+ALTER TABLE Arma
+MODIFY COLUMN evaluacion_arma FLOAT;
+
+ALTER TABLE Arma 
+MODIFY COLUMN idArma INT NOT NULL AUTO_INCREMENT;
+--------------------------------------
+
+-------------MAYO/2020
 ALTER TABLE Publicacion
 MODIFY COLUMN descripcion varchar(2200);
 
@@ -8,3 +19,4 @@ CHANGE url urlPublicacion varchar(500);
 
 ALTER TABLE Publicacion
 ADD COLUMN urlImagen varchar(500);
+--------------------------------------
